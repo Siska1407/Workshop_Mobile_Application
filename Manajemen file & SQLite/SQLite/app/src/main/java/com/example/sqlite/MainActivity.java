2898eec81203abf1e5802e098633feb1553e1aad
Siska Yulia Rahmawati <ladyrose1407@gmail.com>
2021-10-24 11:25:31 +0700

    SQLite

--- Manajemen file & SQLite/SQLite/app/src/main/java/com/example/sqlite/MainActivity.java
+++ Manajemen file & SQLite/SQLite/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -1,95 +1,161 @@
 package com.example.sqlite;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    String[] daftar;
-    ListView ListView01;
-    Menu menu;
-    protected Cursor cursor;
-    DataHelper dbcenter;
-    public static MainActivity ma;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ListView listView;
+    DBHelper helper;
+    LayoutInflater inflater;
+    View dialogView;
+    TextView Tv_Nomor, Tv_Nama, Tv_TempatLahir, Tv_JK, Tv_Tanggal, Tv_Alamat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        Button btn=(Button)findViewById(R.id.button2);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-                // TODO Auto-generated method stub
-                Intent inte = new Intent(MainActivity.this, BuatBiodata.class);
-                startActivity(inte);
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
             }
         });
 
-
-        ma = this;
-        dbcenter = new DataHelper(this);
-        RefreshList();
+        helper = new DBHelper(this);
+        listView = (ListView)findViewById(R.id.list_data);
+        listView.setOnItemClickListener(this);
     }
 
-    public void RefreshList(){
-        SQLiteDatabase db = dbcenter.getReadableDatabase();
-        cursor = db.rawQuery("SELECT * FROM biodata",null);
-        daftar = new String[cursor.getCount()];
-        cursor.moveToFirst();
-        for (int cc=0; cc < cursor.getCount(); cc++){
-            cursor.moveToPosition(cc);
-            daftar[cc] = cursor.getString(1).toString();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-        ListView01 = (ListView)findViewById(R.id.listView1);
-        ListView01.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
-        ListView01.setSelected(true);
-        ListView01.setOnItemClickListener(new OnItemClickListener() {
 
-
-            public void onItemClick(AdapterView arg0, View arg1, int arg2, long arg3) {
-                final String selection = daftar[arg2]; //.getItemAtPosition(arg2).toString();
-                final CharSequence[] dialogitem = {"Lihat Biodata", "Update Biodata", "Hapus Biodata"};
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Pilihan");
-                builder.setItems(dialogitem, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int item) {
-                        switch(item){
-                            case 0 :
-                                Intent i = new Intent(getApplicationContext(), LihatBiodata.class);
-                                i.putExtra("nama", selection);
-                                startActivity(i);
-                                break;
-                            case 1 :
-                                Intent in = new Intent(getApplicationContext(), UpdateBiodata.class);
-                                in.putExtra("nama", selection);
-                                startActivity(in);
-                                break;
-                            case 2 :
-                                SQLiteDatabase db = dbcenter.getWritableDatabase();
-                                db.execSQL("delete from biodata where nama = '"+selection+"'");
-                                RefreshList();
-                                break;
-                        }
-                    }
-                });
-                builder.create().show();
-            }});
-        ((ArrayAdapter)ListView01.getAdapter()).notifyDataSetInvalidated();
+        return super.onOptionsItemSelected(item);
     }
 
+    public void setListView(){
+        Cursor cursor = helper.allData();
+        CustomCursorAdapter customCursorAdapter = new CustomCursorAdapter(this, cursor, 1);
+        listView.setAdapter(customCursorAdapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int i, long x) {
+        TextView getId = (TextView)view.findViewById(R.id.listID);
+        final long id = Long.parseLong(getId.getText().toString());
+        final Cursor cur = helper.oneData(id);
+        cur.moveToFirst();
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Pilih Opsi");
+
+        //Add a list
+        String[] options = {"Lihat Data", "Edit Data", "Hapus Data"};
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:
+                        final AlertDialog.Builder viewData = new AlertDialog.Builder(MainActivity.this);
+                        inflater = getLayoutInflater();
+                        dialogView = inflater.inflate(R.layout.view_data, null);
+                        viewData.setView(dialogView);
+                        viewData.setTitle("Lihat Data");
+
+                        Tv_Nomor = (TextView)dialogView.findViewById(R.id.tv_No);
+                        Tv_Nama = (TextView)dialogView.findViewById(R.id.tv_Nama);
+                        Tv_TempatLahir = (TextView)dialogView.findViewById(R.id.tv_TempatLahir);
+                        Tv_Tanggal = (TextView)dialogView.findViewById(R.id.tv_Tanggal);
+                        Tv_JK = (TextView)dialogView.findViewById(R.id.tv_JK);
+                        Tv_Alamat = (TextView)dialogView.findViewById(R.id.tv_Alamat);
+
+                        Tv_Nomor.setText("Nomor: " + cur.getString(cur.getColumnIndex(DBHelper.row_nomor)));
+                        Tv_Nama.setText("Nama: " + cur.getString(cur.getColumnIndex(DBHelper.row_nama)));
+                        Tv_TempatLahir.setText("Tempat Lahir: " + cur.getString(cur.getColumnIndex(DBHelper.row_tempatLahir)));
+                        Tv_Tanggal.setText("Tanggal Lahir: " + cur.getString(cur.getColumnIndex(DBHelper.row_tglLahir)));
+                        Tv_JK.setText("Jenis Kelamin: " + cur.getString(cur.getColumnIndex(DBHelper.row_jk)));
+                        Tv_Alamat.setText("Alamat: " + cur.getString(cur.getColumnIndex(DBHelper.row_alamat)));
+
+                        viewData.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        viewData.show();
+                }
+                switch (which){
+                    case 1:
+                        Intent iddata = new Intent(MainActivity.this, EditActivity.class);
+                        iddata.putExtra(DBHelper.row_id, id);
+                        startActivity(iddata);
+                }
+                switch (which){
+                    case 2:
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                        builder1.setMessage("Data ini akan dihapus.");
+                        builder1.setCancelable(true);
+                        builder1.setPositiveButton("Hapus", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                helper.deleteData(id);
+                                Toast.makeText(MainActivity.this, "Data Terhapus", Toast.LENGTH_SHORT).show();
+                                setListView();
+                            }
+                        });
+                        builder1.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog alertDialog = builder1.create();
+                        alertDialog.show();
+                }
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setListView();
+    }
 }
